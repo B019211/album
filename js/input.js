3d2b43ee71b56905609cc2b9c6dd409583197117
B019211 <motor.yamada@ntt.com>
2021-01-19 21:43:28 +0900

    second commit

--- js/input.js
+++ js/input.js
@@ -1,7 +1,24 @@
 window.addEventListener('DOMContentLoaded' , 
     function() {
         //(1)ページ本体が読み込まれたタイミングで実行するコード
+        let subject = document.getElementById('subject');
+        let memo = document.getElementById('memo');
+        let save = document.getElementById('save');
         let cancel = document.getElementById('cancel');
+
+        save.addEventListener('click',
+            function(e) {
+                //(1)[保存]ボタンをクリック時に実行するコード
+                e.preventDefault();
+                if(subject.validity.valid === false || 
+                    memo.validity.valid === false) {
+                       //(2)件名、メモが入力されていないときに実行するコード
+                       window.alert('件名、メモはいずれも必須です。');
+                       return;
+                }
+            }, false
+        );
+
         cancel.addEventListener('click' , 
             function() {
                 //(2)[キャンセル]ボタンのクリック時に実行するコード
