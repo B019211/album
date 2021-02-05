50e91e0e384b3d3b8b42ca1bbb2ce0b4d0bd3a91
B019211 <motor.yamada@ntt.com>
2021-02-05 10:05:35 +0900

    今度こそ完成した。

--- js/index.js
+++ js/index.js
@@ -1,7 +1,145 @@
 window.addEventListener('DOMContentLoaded' , 
     function() {
         //(1)ページ本体が読み込まれたタイミングで実行するコード
+        let w_id, gmap, c_point;
+        let m_list = new google.maps.MVCArray();
         let result = document.getElementById('result');
-        result.textContent = 'Hello, JavaScript!!!'
+        let stopwatch = document.getElementById('stopwatch');
+        let showcurrent =document.getElementById('showcurrent');
+
+        let removemarker = function() {
+            m_list.forEach(function(marker, index) {marker.setMap(null); });
+        };
+
+        let showmap = function(e) {
+            removemarker();
+            //(1)メモタイトルをタップした場合の処理を定義
+            let id = e.target.dataset.id;
+            let list = localStorage.getItem('memolist');
+            if(list !== null) {
+                //(2)メモ情報が存在する場合の処理を定義
+                list = JSON.parse(list);
+                let item = list[id];
+                let point = new google.maps.LatLng(
+                    item.latitude, item.longitude);
+                let marker = new google.maps.Marker({
+                    //(3)マーカーの基本情報を定義
+                    map: gmap,
+                    position: point
+                });
+                let msg = `<strong>${h(item.subject)}</strong><br />${h(item.memo)}`;
+                if (item.picture) {
+                    msg += `<br /><img src="${h(item.picture)}" width="100", height="100" />`;
+                }
+                let info = new google.maps.InfoWindow({
+                    //(4)メッセージウィンドウの基本情報を定義
+                    content: msg
+                });
+                google.maps.event.addListener(marker, 'click', 
+                    function() {
+                        //(5)マーカタップ時の挙動を定義
+                        info.open(gmap, marker);
+                    });
+                gmap.setCenter(point);
+                m_list.push(marker);
+            }
+        };
+
+        let removememo = function(e) {
+            removemarker();
+            //(1)メモリストから対象のメモを削除するコード
+            let id =e.target.dataset.id;
+            let list =JSON.parse(localStorage.getItem('memolist'));
+            list = list.filter(function(memo, c_index,ary) {
+                //(2)削除対象のメモを除去
+                return id !== c_index.toString();
+            });
+            localStorage.setItem('memolist', JSON.stringify(list));
+            showmemo();
+        }
+
+        let showmemo = function() {
+            //(1)ローカルストレージからデータを取得するコード
+            let msg = '';
+            let list = localStorage.getItem('memolist');
+            if (list !== null) {
+                //(2)ローカルすれとれーじからmemolistを取得できた場合の処理
+                list = JSON.parse(list);
+                for (let i = 0; i < list.length; i++) {
+                    //(3)リストを表すタグを組み立て
+                    msg += `<li>
+                        <a href="#" class="show" data-id="${h(i)}">
+                            ${h(list[i].subject)}
+                        </a>
+                        <a class="del" href="#" data-id="${h(i)}">×</a>
+                        </li>`;
+                }
+                let r_list = document.getElementById('list');
+                r_list.innerHTML = msg;
+                let subjects = document.querySelectorAll('#list a.show');
+                for (let subject of subjects) {
+                    subject.addEventListener('click', showmap, false);
+                }
+                let deletes = document.querySelectorAll('#list a.del');
+                for(let del of deletes) {
+                    del.addEventListener('click', removememo, false);
+                }
+            }
+        };
+
+        stopwatch.addEventListener('click' , 
+            function(e) {
+                navigator.geolocation.clearWatch(w_id);
+            }, false
+        );
+
+        showcurrent.addEventListener('click', 
+            function(e) {
+                removemarker();
+                gmap.setCenter(c_point);
+            }, false
+        );
+
+        if(navigator.geolocation) {
+            //(1)現在位置を取得するコード
+            w_id = navigator.geolocation.watchPosition (
+                function(pos) {
+                    //(2)位置情報の取得に成功した時の処理
+                    c_point = new google.maps.LatLng(
+                        pos.coords.latitude, pos.coords.longitude);
+                    gmap = new google.maps.Map(
+                        result,
+                        {
+                            zoom: 14,
+                            center: c_point,
+                            mapTypeId: google.maps.MapTypeId.ROADMAP
+                        }
+                    );
+                    sessionStorage.setItem(
+                        'cpos_latitude', pos.coords.latitude);
+                    sessionStorage.setItem(
+                        'cpos_longitude', pos.coords.longitude);
+                },
+                function(err) {
+                    //(1)位置情報の取得に失敗したときの処理
+                    let msgs = [
+                        '',
+                        'Geolocationの利用が許可されていません。',
+                        '位置情報を取得できません。',
+                        '位置情報の取得中にタイムアウトしました。'
+                    ];
+                    result.textContent = msgs[err.code];
+                },
+                {
+                    //(2)位置情報取得のためのオプション
+                    timeout : 7000,
+                    maximumAge : 500,
+                    enableHighAccuracy: false
+                }
+            )
+        } else {
+            window.alert('Geolocation APIに対応したブラウザでアクセスしてください。');
+        }
+        showmemo();
     }, false
 );
--- js/input.js
+++ js/input.js
@@ -1,10 +1,72 @@
 window.addEventListener('DOMContentLoaded' , 
     function() {
         //(1)ページ本体が読み込まれたタイミングで実行するコード
+        let c, o_x = 0, o_y = 0, flag = false;
         let subject = document.getElementById('subject');
         let memo = document.getElementById('memo');
         let save = document.getElementById('save');
         let cancel = document.getElementById('cancel');
+        let board =document.getElementById('board');
+
+        //(1)canvas_APIを利用した処理
+        c = board.getContext('2d');
+
+        //閉域ライン
+        c.beginPath();
+        c.moveTo(250,10);
+        c.lineTo(50,50);
+        c.lineTo(30,150);
+        c.lineTo(170,180);
+        c.lineTo(250,150);
+        c.closePath();
+        //グラデーション
+        let gr = c.createRadialGradient(150, 150, 10, 150, 150, 120);
+        gr.addColorStop(0, '#ff0');
+        gr.addColorStop(0.5, '#f0f');
+        gr.addColorStop(1, '#0ff');
+        c.fillStyle = gr;
+        c.lineWidth = 3;
+        c.stroke();
+        c.fill();
+
+        //2次ビジェ曲線
+        c.moveTo(200, 10);
+        c.quadraticCurveTo(10, 100,250 ,150);
+        c.strokeStyle = '#f00';
+        c.lineWidth = 3;
+        c.stroke();
+
+        //3次ビジェ曲線
+        c.moveTo(40, 50);
+        c.bezierCurveTo(50, 150, 150, 350, 50, 120, 150)
+        c.strokeStyle = '#f00';
+        c.lineWidth = 3;
+        c.stroke();
+
+        //円
+        c.arc(150, 100, 80, 0, 2 * Math.PI, true);
+        c.strokeStyle = '#f00';
+        c.fillStyle = '#cc5';
+        c.lineWidth = 3;
+        c.stroke();
+        c.fill();
+
+        //文字
+        c.font = '30px, sans-serif';
+        c.fillText('こんにちは', 70, 50);
+        c.strokeText('こんにちは', 70, 50);
+
+        c.fillStyle = '#00f';
+        c.lineWidth = 3;
+
+        let img = new Image();
+        img.src = 'image/ham.jpg';
+        img.addEventListener(
+            'load',
+            function(e) {
+                c.drawImage(img, 80, 80, 120, 100);
+            }
+        );
 
         save.addEventListener('click',
             function(e) {
@@ -16,6 +78,32 @@ window.addEventListener('DOMContentLoaded' ,
                        window.alert('件名、メモはいずれも必須です。');
                        return;
                 }
+                //(1)セッションストレージから現在位置を取得するコード
+                let cpos_latitude = sessionStorage.getItem('cpos_latitude');
+                let cpos_longitude = sessionStorage.getItem('cpos_longitude');
+                if(cpos_latitude == null || cpos_longitude == null) {
+                    window.alert('トップページからアクセスしなおしてください。');
+                    location.href = 'index.html';
+                }
+                //(2)ローカルストレージにメモを登録するコード
+                let list = localStorage.getItem('memolist');
+                if(list === null) {
+                    list =[];
+                } else {
+                    list = JSON.parse(list);
+                }
+                list.push({
+                    //(3)ローカルストレージに保存する内容
+                    latitude: cpos_latitude,
+                    longitude: cpos_longitude,
+                    subject: subject.value,
+                    memo: memo.value,
+                    picture: board.toDataURL(),
+                    updated: new Date()
+                });
+                list = JSON.stringify(list);
+                localStorage.setItem('memolist', list);
+                location.href = 'index.html';
             }, false
         );
 
@@ -25,5 +113,51 @@ window.addEventListener('DOMContentLoaded' ,
                 location.href = 'index.html';
             }, false
         );
+
+        let ondown = function(e) {
+            //(1)タッチ開始/マウスダウン時の処理
+            e.preventDefault();
+            flag = true;
+            if (e.touches) {e = e.touches[0];}
+            let c_rect = e.target.getBoundingClientRect();
+            o_x = e.clientX - c_rect.left;
+            o_y = e.clientY - c_rect.top;
+        };
+
+        let onup = function(e) {
+            //(2)タッチ終了/マウスアップ時の処理
+            e.preventDefault();
+            flag = false;
+        };
+
+        let onmove = function(e) {
+            //(3)タッチ移動中/マウス移動中時の処理
+            e.preventDefault();
+            if (flag) {
+                //(6)マウスが押されている場合の処理
+                if(e.touches) { e = e.touches[0]; }
+                let c_rect = e.target.getBoundingClientRect();
+                let x = e.clientX - c_rect.left;
+                let y = e.clientY - c_rect.top;
+                c.beginPath();
+                c.moveTo(o_x, o_y) ;
+                c.lineTo(x, y);
+                c.stroke();
+                o_x = x;
+                o_y = y;               
+            }
+        };
+
+        if(window.ontouchstart === undefined) {
+            //(4)タッチイベントに対応していない場合の処理
+            board.addEventListener('mousedown', ondown, false);
+            board.addEventListener('mouseup', onup, false);
+            board.addEventListener('mousemove', onmove, false);
+        } else {
+            //(5)タッチイベントに対応している場合の処理
+            board.addEventListener('touchdown', ondown, false);
+            board.addEventListener('touchup', onup, false);
+            board.addEventListener('touchmove', onmove, false);      
+        }
     }, false
 );
